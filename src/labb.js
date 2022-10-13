49f882a3020d60659528e398e0055fc176ab7701
hansopoien <93471661+hansopoien@users.noreply.github.com>
2022-10-13 20:38:28 +0200

    test j

--- src/labb.js
+++ src/labb.js
@@ -1,12 +1,54 @@
 // modifiera sum() tills testet blir godkänt!
 function sum(a, b) {
-  console.log("funct sum");
+  console.log(sum);
+  return a + b;
 }
 
-function myOwnMultiplyFunction() {}
+function myOwnMultiplyFunction(a, b) {
+  return a * b;
+}
 
 // denna måste exporteras längst ned i denna fil! annars kan labb.test.js inte importera den.
-function isBelowZero() {}
+function isBelowZero(value) {
+  if (value < 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function round(number){
+  return Math.round(number)
+}
+
+function addingUp(num) {
+  let a = 0;
+  let b = 0;
+  for (let index = 0; index < num; index++) {
+    a++
+    b += a;
+  }
+  return b;
+}
+
+function findMinMax(arrayNumbers) {
+  let min = Math.min(...arrayNumbers);
+  let max = Math.max(...arrayNumbers);
+  let minMax = {min: min, max: max}
+  return minMax;
+}
+
+function afterXmasEve2020(date) {
+  let input = new Date(date)
+  const xmasEve2020 = new Date('2020-12-24')
+  if (input >= xmasEve2020) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 // fortsätt skapa alla lösningar som beskrivs i labb.test.js och exportera dem!
 
@@ -15,11 +57,11 @@ function isBelowZero() {}
 // https://eloquentjavascript.net/10_modules.html#h_N33QHgUxbG how require and modules work in JavaScript
 module.exports.sum = sum;
 module.exports.multiply = myOwnMultiplyFunction;
-module.exports.isBelowZero = undefined;
-module.exports.round = undefined;
-module.exports.addingUp = undefined;
-module.exports.findMinMax = undefined;
-module.exports.afterXmasEve2020 = undefined;
+module.exports.isBelowZero = isBelowZero;
+module.exports.round = round;
+module.exports.addingUp = addingUp;
+module.exports.findMinMax = findMinMax;
+module.exports.afterXmasEve2020 = afterXmasEve2020;
 module.exports.sortByStringLength = undefined;
 module.exports.charCounter = undefined;
 module.exports.numbersOnly = undefined;
